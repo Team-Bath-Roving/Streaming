dcb9cbb1c937b10d689816eb2fd1dd8fe194a698
PixelMutation <charlie.e.spencer@gmail.com>
2023-03-13 19:37:35 +0000

    update

--- sendPicamStream.sh
+++ sendPicamStream.sh
@@ -1,1 +1,4 @@
+# find available resolutions and framerates with libcamera-vid --list-cameras
 libcamera-vid -t 0 --inline --width 1296 --height 972 -o - | nc -k -l 8081
+# Alternatively, using gst (could change sink to RTSP or RTC to use udp)
+libcamera-vid -t 0 --inline --width 1296 --height 972 -o - | gst-launch-1.0 fdsrc ! tcpsink host=0.0.0.0 port=8081
