aac8d25c2600f41ee4f2fcef16ee82f381c4fba4
PixelMutation <charlie.e.spencer@gmail.com>
2023-07-16 10:45:49 +0100

    Upgrade server to use a class

--- StreamServer.py
+++ StreamServer.py
@@ -1,4 +1,3 @@
-
 import socket
 import time
 
@@ -6,26 +5,53 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
 
-from signal import signal, SIGINT
-running=True
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration({
-    "size": (1296, 972)},)
-picam2.configure(video_config)
-encoder = H264Encoder(bitrate=1000000,repeat=True,iperiod=5)
-
-def handler(signal_received,frame):
-    picam2.stop_recording()
-    running=False
-
-signal(SIGINT, handler)
-
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect(("192.168.68.103", 8081))
-    stream = sock.makefile("wb")
-    picam2.start_recording(encoder, FileOutput(stream))
-    while(running):
-        time.sleep(1)
-        # for i in range(0,320):
-        #     time.sleep(1)
-        #     picam2.set_controls({"Contrast":i/10})
+class StreamServer:
+    cam_list=None
+    def __init__(self,model):
+        # List available devices
+        self.usb=False
+        self.cam=None
+        if StreamServer.cam_list is None: # first instantiation gets list of cameras
+            StreamServer.scan()
+        for idx,camera in enumerate(StreamServer.cam_list):
+            # Find camera with corresponding model name
+            print(camera["Model"])
+            if model in camera["Model"]:
+                if not "Active" in camera: # skip this one if it's already been claimed
+                    # Determine if CSI or USB (YUV or YUYV/MJPEG)
+                    if "usb" in camera["Id"]:
+                        usb=True
+                    # Initialise camera
+                    self.cam=Picamera2(idx)
+                    camera["Active"]=True # add key to show this has been claimed (allows multiple cams with same model)
+        if self.cam is None:
+            raise Exception(f"Failed to find camera with model name {model}")
+    def scan():
+        StreamServer.cam_list=Picamera2.global_camera_info()
+    def configure(self,width,height,format=None):
+        config={"size":(width,height)}
+        if not format is None:
+            config["format"]=format
+        self.cam.configure(self.cam.create_video_configuration(config))
+    def start(self,ip,port,multicast=True):
+        if not self.usb:
+            self.encoder=H264Encoder(100000,repeat=True,iperiod=5)
+        if multicast:
+            self.sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+        else:
+            self.sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+        self.sock.connect((ip, port))
+        self.stream = self.sock.makefile("wb")
+        self.cam.start_recording(self.encoder, FileOutput(self.stream))
+    def stop(self):
+        self.cam.stop_recording()
+        self.sock.close()
+    def set_bitrate(self,bitrate):
+        self.encoder.bitrate=bitrate
+        self.encoder.stop()
+        self.encoder.start()
+    def set_controls(self,controls_dict):
+        self.cam.set_controls(controls_dict)
+    def set_exposure(self,exposure):
+        self.set_controls({"ExposureTime": exposure})
---
+++ exampleServer.py
@@ -0,0 +1,28 @@
+
+import time
+
+from StreamServer import StreamServer
+
+from signal import signal, SIGINT
+running=True
+
+MCAST_GRP = '239.1.1.1'
+
+stereo=StreamServer("ov564") # system finds the camera based upon the model number (assumes no duplicates)
+stereo.configure(1296,972)
+#stereo.configure(640,480)
+stereo.start(MCAST_GRP,5008,True) # using a multicast address 224.1.1.1:5008
+stereo.set_bitrate(5000000)
+i=10
+while(running):
+    time.sleep(0.01)
+    i+=10
+    stereo.set_exposure(i)
+
+
+
+def handler(signal_received,frame):
+    stereo.stop()
+    running=False
+
+signal(SIGINT, handler)
