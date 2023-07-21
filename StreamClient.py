dde529660933fe02875a9c527a28aad140baef3e
PixelMutation <charlie.e.spencer@gmail.com>
2023-07-21 20:24:34 +0100

    add cropping

--- StreamClient.py
+++ StreamClient.py
@@ -4,18 +4,25 @@ import numpy as np
 # import multiprocessing as mp
 import threading
 
+NO_CROP=0
+LEFT_CROP=1
+RIGHT_CROP=2
+
 class StreamClient:
     thread=None
     process=None
     frame=None
     end=False
+    crop=None
     options={
         "udp":f"?buffer_size=1000&fifo_size={3*4096}",
         "tcp":"?tcp_nodelay=1",
         "rtsp":"?buffer_size=1000&reorder_queue_size=100",
         "rtp":"",
     }
-    def __init__(self,name,host,type,port,width,height,stereo=False):
+    def __init__(self,name,host,type,port,width,height,rotate=0,stereo=False,crop=NO_CROP):
+        self.crop=crop
+        self.rotate=rotate
         self.name=name
         self.host=host
         self.port=port
@@ -48,7 +55,7 @@ class StreamClient:
             '-vf',f"scale={self.width}:{self.height}",
             '-fflags', "nobuffer",
             '-f', 'rawvideo',      # Get rawvideo output format.
-            '-pix_fmt', 'bgr24',   # Set BGR pixel format
+            '-pix_fmt', 'rgb24',   # Set BGR pixel format
             'pipe:']
         try:
             self.process.stdout.close()  # Closing stdout terminates FFmpeg sub-process.
@@ -60,7 +67,14 @@ class StreamClient:
     def read(self):
         raw_frame = self.process.stdout.read(self.width*self.height*3)
         # print(len(raw_frame))
-        self.frame = np.frombuffer(raw_frame, np.uint8).reshape((self.height, self.width, 3))
+        raw_frame = np.frombuffer(raw_frame, np.uint8).reshape((self.height, self.width, 3))
+        if self.crop==LEFT_CROP:
+            self.frame=raw_frame[:,:self.width//2,:]
+        elif self.crop==RIGHT_CROP:
+            self.frame=raw_frame[:,self.width//2:,:]
+        else:
+            self.frame=raw_frame
+        self.frame=np.rot90(self.frame,self.rotate)
         # print(len(self.frame))
         return self.frame
     def run(self):
