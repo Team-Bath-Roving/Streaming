dbc4657ccc8b5c29352fe65faca4ea4627b0e06a
PixelMutation <34405284+PixelMutation@users.noreply.github.com>
2023-07-24 17:42:52 +0100

    Update exampleServer.py

--- exampleServer.py
+++ exampleServer.py
@@ -2,27 +2,41 @@
 import time
 
 from StreamServer import StreamServer
+from Comms.Output import Output
 
 from signal import signal, SIGINT
-running=True
 
-MCAST_GRP = '239.1.1.1'
+# config
+MULTICAST=True
+IP = '239.1.1.1' # UDP multicast IP
+PORT=5008
+MODEL="ov5647" # camera model name (find using libcamera-vid --list-cameras)
+WIDTH=1296
+HEIGHT=972
+NAME="camera" # stream name for display in console
 
-stereo=StreamServer("ov5647") # system finds the camera based upon the model number (assumes no duplicates)
-stereo.configure(1296,972)
-#stereo.configure(640,480)
-stereo.start(MCAST_GRP,5008,True) # using a multicast address 224.1.1.1:5008
-stereo.set_bitrate(5000000)
+out=Output("None") # console output, with optional TCP forwarding
+
+# Initialise stream
+stream=StreamServer(out,MODEL,NAME) # system finds the camera based upon the model number (assumes no duplicates)
+stream.configure(WIDTH,HEIGHT)
+stream.start(IP,PORT,MULTICAST) # using a multicast address 224.1.1.1:5008
+stream.set_bitrate(5000000)
+
+# Example of script to change settings of camera
 i=10
-while(running):
+running=True
+while(running): # stream ends once this thread ends
     time.sleep(0.01)
     i+=10
-    stereo.set_exposure(i)
+    stream.set_exposure(i)
 
 
-
+# exit handler
 def handler(signal_received,frame):
-    stereo.stop()
+    stream.stop()
     running=False
 
 signal(SIGINT, handler)
+
+
